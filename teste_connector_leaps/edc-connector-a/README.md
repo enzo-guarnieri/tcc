c3b26553a86457594b08c2bf1517e8e1e4f2b21f
enzo.guarnieri <enzo.guarnieri@mackenzie.br>
2026-03-02 23:03:01 -0300

    transfer + docker

--- teste_connector_leaps/edc-connector-a/README.md
+++ teste_connector_leaps/edc-connector-a/README.md
@@ -2,3 +2,11 @@
 ./gradlew :connector:shadowJar
 
 java -jar connector/build/libs/basic-connector.jar
+
+java -Dedc.fs.config=config.properties -jar connector/build/libs/basic-connector.jar
+
+------ transfer -------
+
+java -Dedc.fs.config=configuracoes/consumer-configuration.properties -jar connector/build/libs/basic-connector.jar
+
+java -Dedc.fs.config=configuracoes/provider-configuration.properties -jar connector/build/libs/basic-connector.jar
--- teste_connector_leaps/edc-connector-a/connector/build.gradle.kts
+++ teste_connector_leaps/edc-connector-a/connector/build.gradle.kts
@@ -21,22 +21,47 @@ plugins {
 
 dependencies {
     implementation(libs.edc.boot)
+    implementation(libs.jakarta.rsApi)
+
     implementation(libs.edc.runtime.core)
     implementation(libs.edc.connector.core)
-
-    implementation(libs.edc.configuration.filesystem)
-
+    implementation(libs.edc.control.api.configuration)
+    implementation(libs.edc.control.plane.api.client)
+    implementation(libs.edc.control.plane.api)
+    implementation(libs.edc.control.plane.core)
+    implementation(libs.edc.token.core)
+    implementation(libs.edc.dsp)
     implementation(libs.edc.http)
+    implementation(libs.edc.configuration.filesystem)
+    implementation(libs.edc.iam.mock)
+    implementation(libs.edc.management.api)
+    implementation(libs.edc.transfer.data.plane.signaling)
+    implementation(libs.edc.validator.data.address.http.data)
 
-    implementation(libs.jakarta.rsApi)
+    implementation(libs.edc.edr.cache.api)
+    implementation(libs.edc.edr.store.core)
+    implementation(libs.edc.edr.store.receiver)
+
+    implementation(libs.edc.data.plane.selector.api)
+    implementation(libs.edc.data.plane.selector.core)
+
+    implementation(libs.edc.data.plane.self.registration)
+    implementation(libs.edc.data.plane.signaling.api)
+    implementation(libs.edc.data.plane.core)
+    implementation(libs.edc.data.plane.http)
+    implementation(libs.edc.data.plane.iam)
 }
 
 application {
     mainClass.set("org.eclipse.edc.boot.system.runtime.BaseRuntime")
 }
 
+var distTar = tasks.getByName("distTar")
+var distZip = tasks.getByName("distZip")
+
 tasks.withType<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar> {
     mergeServiceFiles()
     archiveFileName.set("basic-connector.jar")
+    dependsOn(distTar, distZip)
     duplicatesStrategy = DuplicatesStrategy.INCLUDE
 }
